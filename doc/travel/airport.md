4b89824bdba824ab0bca7d53b03898253936d83a
Zion Miller <djzionmiller@gmail.com>
2022-11-19 14:22:27 -0800

    changed args from positional to keyword, updated docs to reflect changes

--- doc/travel/airport.md
+++ doc/travel/airport.md
@@ -1,6 +1,6 @@
 # Faker::Travel::Airport
 
 ```ruby
-Faker::Movies::Travel::Airport.name('large', 'united_states') #=> "Los Angeles International Airport"
-Faker::Movies::Travel::Airport.iata('large', 'united_states') #=> "LAX"
+Faker::Movies::Travel::Airport.name(size: 'large', region: 'united_states') #=> "Los Angeles International Airport"
+Faker::Movies::Travel::Airport.iata(size: 'large', region: 'united_states') #=> "LAX"
 ```
--- lib/faker/travel/airport.rb
+++ lib/faker/travel/airport.rb
@@ -14,10 +14,10 @@ module Faker
         # @retrun [String]
         #
         # @example
-        # Faker::Travel::Airport.name('large', 'united_states') => "Los Angeles International Airport"
+        # Faker::Travel::Airport.name(size: 'large', region: 'united_states') => "Los Angeles International Airport"
         #
         # @faker.version next
-        def name(size, region)
+        def name(size:, region:)
           fetch("airport.#{region}.#{size}")
         rescue I18n::MissingTranslationData
           p 'valid arguments are size && region -> US has size large medium small, EU has size large medium -- united_states || european_union'
@@ -33,10 +33,10 @@ module Faker
         # @retrun [String]
         #
         # @example
-        # Faker::Travel::Airport.iata('large', 'united_states') => "LAX"
+        # Faker::Travel::Airport.iata(size: 'large', region: 'united_states') => "LAX"
         #
         # @faker.version next
-        def iata(size, region)
+        def iata(size:, region:)
           fetch("airport.#{region}.iata_code.#{size}")
         rescue I18n::MissingTranslationData
           p 'valid arguments are size && region -> US has size large medium small, EU has size large medium -- united_states || european_union'
